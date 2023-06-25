0af8284ec023ffae23da3c140e6daf2416a74684
ni3mumbaikar <nitinkale035@gmail.com>
2023-06-25 20:46:57 +0530

    Initial commit

---
+++ src/test/kotlin/ind/ni3mumbaikar/microservices/core/CoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package ind.ni3mumbaikar.microservices.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
